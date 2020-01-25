fc3424ef69fa510db6da1aaeb2c8bd5b4044b462
samarth-meghwani <60034215+samarth-meghwani@users.noreply.github.com>
2020-01-25 15:36:20 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# game-object-collision-algorithm
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,52 @@
+var fixedRect,movingRect;
+
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600,400,50,80)
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+obj1 = createSprite(100,100,50,50);
+obj1.shapeColor = "green";
+obj2 = createSprite(200,100,50,50);
+obj2.shapeColor = "green";
+obj3 = createSprite(300,100,50,50);
+obj3.shapeColor = "green";
+obj4 = createSprite(400,100,50,50);
+obj4.shapeColor = "green"
+}
+
+
+function draw() {
+  background(0,0,0);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  
+  
+ if (isTouching(movingRect,obj2)){
+  movingRect.shapeColor = "blue";
+  obj2.shapeColor = "blue";
+ }
+ else{
+  movingRect.shapeColor = "green";
+  obj2.shapeColor = "green";
+ }
+
+  drawSprites();
+}
+function isTouching(object1,object2){
+  if(object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y <  object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2){
+    return true;  
+    }
+  
+  else {
+  return false;
+  }
+}
